7a02f2f22343eec823fce524210fab95788735f4
EvelynEngineer <rdaulton@grammatech.com>
2023-11-06 15:51:45 +0000

    split codebase

---
+++ second.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void goAndUse(char *x, char *y, int t)
+{
+	int ret = rand();
+	
+	if (ret)
+	{
+		free(x);
+		return;
+	}
+	else
+	{
+		//free(x);
+		// leak - no free() on this path
+		// return;
+	}
+}
+
+
+void memoryLeak()
+{
+	// null pointer dereference - malloc may fail
+	char *ptr = (char*)malloc(24);
+	
+	// buffer overrun - ptr not large enough for string copy
+	strcpy(ptr, "Scirs Ltd");
+	
+	goAndUse(ptr, ptr, 10);	
+}
+		
+
